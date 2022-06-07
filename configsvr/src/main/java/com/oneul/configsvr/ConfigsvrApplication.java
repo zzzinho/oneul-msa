d8e0939f80fbe8a0852b524f868d48a997bf6843
jinho jeong <hahoh0013@gmail.com>
2022-06-07 13:22:45 +0900

    init

---
+++ configsvr/src/main/java/com/oneul/configsvr/ConfigsvrApplication.java
@@ -0,0 +1,15 @@
+package com.oneul.configsvr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigsvrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigsvrApplication.class, args);
+	}
+
+}

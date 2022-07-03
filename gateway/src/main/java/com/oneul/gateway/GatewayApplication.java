ccfc0cd76e2dce41e2ee548727ce2ac2edd5ecad
jinho jeong <hahoh0013@gmail.com>
2022-07-03 17:07:59 +0900

    gateway routes config

--- gateway/src/main/java/com/oneul/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/oneul/gateway/GatewayApplication.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
 
+@RefreshScope
 @EnableDiscoveryClient
 @SpringBootApplication
 public class GatewayApplication {
